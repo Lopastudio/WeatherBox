45ddd467c94ad6cf94ae76b9e58e27f6a8d601a1
Patrik <ockonko@gmail.com>
2023-07-06 22:33:28 +0200

    testing line

--- main.py
+++ main.py
@@ -1,3 +1,8 @@
 # Started on 6.7.2023 at 10:30PM
 # Copyright (c) Patrick 2023
 # Licensed under the Apache License, Version 2.0 (the "License");
+
+from gpiozero import CPUTemperature
+
+cpu = CPUTemperature()
+print(cpu.temperature)
