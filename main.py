8345ad5dbd2ab2463b71024a613e17b75c518e88
Patrik <ockonko@gmail.com>
2023-07-06 22:35:53 +0200

    .

--- main.py
+++ main.py
@@ -2,7 +2,20 @@
 # Copyright (c) Patrick 2023
 # Licensed under the Apache License, Version 2.0 (the "License");
 
-from gpiozero import CPUTemperature
+from flask import Flask, render_template
+import os
 
-cpu = CPUTemperature()
-print(cpu.temperature)
+app = Flask(__name__)
+
+def get_cpu_temperature():
+    # Command to get the CPU temperature on a Raspberry Pi
+    temp = os.popen("vcgencmd measure_temp").readline()
+    return temp.replace("temp=", "")
+
+@app.route('/')
+def index():
+    cpu_temp = get_cpu_temperature()
+    return render_template('index.html', cpu_temp=cpu_temp)
+
+if __name__ == '__main__':
+    app.run(debug=True)
