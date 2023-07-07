69ef8ed5a3eb610246066c1d8b81b374b61e2fb3
Patrik <79789176+Lopastudio@users.noreply.github.com>
2023-07-07 09:04:42 +0200

    Adding graph

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-# Started on 6.7.2023 at 10:30PM
-# Copyright (c) Patrick 2023
-# Licensed under the Apache License, Version 2.0 (the "License");
-
-from flask import Flask, render_template
+from flask import Flask, render_template, jsonify
 import os
+from datetime import datetime
 
 app = Flask(__name__)
 
@@ -11,10 +8,32 @@ def get_cpu_temperature():
     temp = os.popen("vcgencmd measure_temp").readline()
     return temp.replace("temp=", "")
 
+def save_temperature(temperature):
+    timestamp = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+    with open("temperature_data.txt", "a") as f:
+        f.write(f"{timestamp},{temperature}\n")
+
 @app.route('/')
 def index():
     cpu_temp = get_cpu_temperature()
+    save_temperature(cpu_temp)
     return render_template('index.html', cpu_temp=cpu_temp)
 
+@app.route('/temperature_data')
+def temperature_data():
+    timestamps = []
+    temperatures = []
+    with open("temperature_data.txt", "r") as f:
+        for line in f:
+            data = line.strip().split(",")
+            timestamps.append(data[0])
+            temperatures.append(float(data[1]))
+    
+    data = {
+        'timestamps': timestamps,
+        'temperatures': temperatures
+    }
+    return jsonify(data)
+
 if __name__ == '__main__':
-    app.run(debug=True, host="192.168.1.139")
+    app.run(debug=True, host="0.0.0.0") #192.168.1.139
